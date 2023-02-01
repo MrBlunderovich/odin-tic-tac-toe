fbad000772ddc2ec8143f606f173b86be8090a05
Maxim <334890@gmail.com>
2023-02-01 18:59:04 +1000

    Make board cell nodes permanent and add listeners

--- index.js
+++ index.js
@@ -1,17 +1,7 @@
 //
 const GameBoard = (function () {
   const _container = document.querySelector(".container");
-  const _board = [
-    { mark: "" },
-    { mark: "X" },
-    { mark: "" },
-    { mark: "" },
-    { mark: "" },
-    { mark: "O" },
-    { mark: "O" },
-    { mark: "O" },
-    { mark: "" },
-  ];
+  const _board = ["", "", "", "", "", "", "", "", ""];
   const _lines = [
     [0, 1, 2],
     [3, 4, 5],
@@ -31,11 +21,11 @@ const GameBoard = (function () {
   const _checkForWin = function () {
     for (let line of _lines) {
       if (
-        _board[line[0]].mark !== "" &&
-        _board[line[0]].mark === _board[line[1]].mark &&
-        _board[line[2]].mark === _board[line[1]].mark
+        _board[line[0]] !== "" &&
+        _board[line[0]] === _board[line[1]] &&
+        _board[line[2]] === _board[line[1]]
       ) {
-        _gameOver = _board[line[0]].mark;
+        _gameOver = _board[line[0]];
         console.log(_gameOver + " win on line: " + line);
         return;
       }
@@ -47,7 +37,7 @@ const GameBoard = (function () {
       console.log("game is over");
       return;
     }
-    if (_board[index].mark !== "") {
+    if (_board[index] !== "") {
       console.log("cell is already checked");
       return;
     }
@@ -56,30 +46,57 @@ const GameBoard = (function () {
       console.log("invalid mark or cell index");
       return;
     }
-    _board[index].mark = mark;
-    _renderCells();
+    _board[index] = mark;
+    _fillCells();
     _checkForWin();
   };
 
   const resetBoard = function () {
-    _board.forEach((cell) => (cell.mark = ""));
+    for (let i = 0; i < _board.length; i++) {
+      _board[i] = "";
+    }
     _gameOver = false;
-    _renderCells();
+    _fillCells();
   };
 
-  const _generateCells = function () {
-    //_resetBoard()
-    return _board
+  const generateCells = function () {
+    const boardCells = _board
       .map((item, index) => {
-        return `<div data-index="${index}" class="cell">${item.mark}</div>`;
+        return `<div data-index="${index}" class="cell">${item}</div>`;
       })
       .join("");
+    _container.innerHTML = boardCells;
+    const cellsCollection = document.querySelectorAll(".cell");
+    cellsCollection.forEach((cell) => {
+      cell.addEventListener("click", handleCellClick);
+    });
+
+    const spaceReset = function (event) {
+      console.log(event);
+      if (event.keyCode === 32) {
+        resetBoard();
+      }
+    };
+    document.addEventListener("keydown", spaceReset);
   };
 
-  const _renderCells = function () {
-    _container.innerHTML = _generateCells();
+  const _fillCells = function () {
+    const cellsCollection = document.querySelectorAll(".cell");
+    console.log("_fillCells");
+    console.log(_board);
+    cellsCollection.forEach((cell) => {
+      const cellIndex = +cell.dataset.index;
+      cell.textContent = _board[cellIndex];
+    });
   };
-  return { resetBoard, markCell, isGameOver };
+
+  return { generateCells, resetBoard, markCell, isGameOver };
 })();
 
-GameBoard.resetBoard();
+GameBoard.generateCells();
+
+function handleCellClick(event) {
+  const cellIndex = event.target.dataset.index;
+  console.log(cellIndex);
+  GameBoard.markCell(cellIndex, "X");
+}
